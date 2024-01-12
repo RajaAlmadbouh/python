c535e1e1bdef45c7f9f26aa3b9e6b394aef90148
RajaAlmadbouh <146444351+RajaAlmadbouh@users.noreply.github.com>
2024-01-12 23:56:43 +0300

    taskString

--- page1.py
+++ page1.py
@@ -395,6 +395,47 @@ print(var.isidentifier())'''
 ''''''''''''''''''''''''''''''''''''''''''''''''''''''
 ''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
+
+'''---------------------String-------------------------'''
+
+'''string = "Hello, word!"
+
+print(string.index("H"))
+print(string[:])
+print(string[5:9])
+print(string[::-1])
+print(string[::-2])
+print(string[:7])
+print(string[-9:-1])
+print(ord("A")) # 65 (According to ascii code )
+print(chr(65)) # A (is the inverse of the ord() function)
+print(string.upper())# HELLO, WORD! (Is method in Python is used to convert all characters in a string to uppercas)
+print(string.lower())# hello, word! (Is method in Python is used to convert all characters in a string to lowercas)
+print(string.capitalize())# Hello, word! (Is method in Python is used to capitalize the first character of a string)
+print(string.title())# hello, word! (Is method in Python is used to capitalize the first letter of each word in a string)
+print(string.find("H"))# 0 (method in Python is used to search for a substring within a string, and returns the index of the first occurrence of the substring, if found. If the substring is not found, it returns -1)
+print(string.find("h"))# -1
+
+
+
+
+
+
+for i in range(0,len(string)):
+    if string[i]==" ":
+        print(i)
+        break
+    else:
+        print("not found character")'''
+
+
+
+''''''''''''''''''''''''''''''''''''''''''''''''''''''
+''''''''''''''''''''''''''''''''''''''''''''''''''''''
+''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+'''---------------------List-------------------------'''
+
 list1 = [5,1.3,"python",[4,8,2.9]]
 list2 = [11,7,55,90,0,25]
 list3 = ["r","a","j","a"]
@@ -444,3 +485,154 @@ list4.insert(1,"Zekoo")
 
 print(list4)
 print(list4.index([1.7,8]))
+print(list4.index("Raja"))
+
+''''''''''''''''''''''''''''''''''''''''''''''''''''''
+''''''''''''''''''''''''''''''''''''''''''''''''''''''
+''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+'''def createN (s1,s2):
+    if len(s1)%2 == 0:
+        return s1[:int(len(s1)/2)] + s2
+    else:
+        return s1[:int(len(s1)/2+1)] + s2
+
+
+s1 = input("string1 = ")
+s2 = input("string2 = ")
+
+print(createN(s1,s2))'''
+
+
+''''''''''''''''''''''''''''''''''''''''''''''''''''''
+''''''''''''''''''''''''''''''''''''''''''''''''''''''
+''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+'''def cahrr (s1):
+    Lo = ""
+    Up = ""
+    for i in s1:
+        if 65 <= ord(i) and ord(i) <= 90:
+            Up += i
+        elif 97 <= ord(i) and ord(i) <= 122:
+            Lo += i
+    print(Lo + Up)
+            
+
+s1 = input("Enter the string : ")
+cahrr(s1)'''
+
+
+''''''''''''''''''''''''''''''''''''''''''''''''''''''
+''''''''''''''''''''''''''''''''''''''''''''''''''''''
+''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+
+'''import string
+
+def cahrr (s1):
+    Chars = 0
+    Digits = 0
+    Symbol = 0
+    for i in s1:
+        if 65 <= ord(i) <= 90 or 97 <= ord(i) <= 122:
+            Chars += 1
+        elif i in string.punctuation:
+            Symbol += 1
+        elif i in "0123456789":
+            Digits += 1
+
+    print("Caracter = ",Chars)
+    print("Digits = ",Digits)
+    print("Symbol = ",Symbol)
+            
+
+s1 = input("Enter the string : ")
+cahrr(s1)'''
+
+
+''''''''''''''''''''''''''''''''''''''''''''''''''''''
+''''''''''''''''''''''''''''''''''''''''''''''''''''''
+''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+'''def findd (s1,s2):
+    if s2.find(s1) != -1 :
+        print("True")
+    else:
+        print("False")
+
+s1= input("enter string1 = ")
+s2= input("enter string2 = ")
+findd(s1,s2)'''
+
+''''''''''''''''''''''''''''''''''''''''''''''''''''''
+''''''''''''''''''''''''''''''''''''''''''''''''''''''
+''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+'''def findd(s1):
+    count = 0
+    chick = True
+
+    while chick:
+        if s1.find("SDK") != -1 or s1.find("sdk") != -1:
+            count += 1
+        else:
+            chick = False
+    print(count)
+
+s1= input("Enter String = ")
+findd(s1)'''
+
+'''def findd(s1):
+    count = 0
+    start_index = 0
+
+    while True:
+        index = s1.lower().find("sdk", start_index)
+        if index == -1:
+            break
+        count += 1
+        start_index = index + 1
+
+    print("Number of occurrences of 'SDK' or 'sdk' in the string:", count)
+
+s1 = input("Enter String: ")
+findd(s1)'''
+
+''''''''''''''''''''''''''''''''''''''''''''''''''''''
+''''''''''''''''''''''''''''''''''''''''''''''''''''''
+''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+'''def summ (s1):
+    sum = 0
+    count = 0
+    charr = "0123456789"
+    for i in s1 :
+        if i in charr:
+            sum += int(i)
+            count += 1
+    print("Sum is: 38 Average is = ",sum/count)
+    
+s1 = input("Enter String : ")
+summ(s1)'''
+
+''''''''''''''''''''''''''''''''''''''''''''''''''''''
+''''''''''''''''''''''''''''''''''''''''''''''''''''''
+''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+'''import string
+
+def convert(s1):
+    for i in s1:
+        if i in string.punctuation:
+            i = "#"
+        print(i,end="")
+
+s1 = input("Enter String = ")
+convert(s1)'''
+
+
+''''''''''''''''''''''''''''''''''''''''''''''''''''''
+''''''''''''''''''''''''''''''''''''''''''''''''''''''
+''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
