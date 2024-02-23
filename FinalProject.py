9eca19486232b4cecef94f200c9fc00cc5848947
RajaAlmadbouh <146444351+RajaAlmadbouh@users.noreply.github.com>
2024-02-23 22:00:27 +0300

    .

--- FinalProject.py
+++ FinalProject.py
@@ -1,4 +1,4 @@
-import tkinter as tk
+``import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 from PIL import Image, ImageTk
@@ -668,3 +668,4 @@ except ValueError as e:
 
 
 MainFunction()
+``
